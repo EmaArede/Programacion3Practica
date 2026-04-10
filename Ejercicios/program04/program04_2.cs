4ff78b094951ede2490a9ff559b9f47e5cdb0b2a
Emanuel Arede Suárez <eearedesuarez@gmail.com>
2026-04-10 10:35:54 -0300

    este programa diferencia string de char

---
+++ Ejercicios/program04/program04_2.cs
@@ -0,0 +1,14 @@
+// programa para diferenciar string de char
+using System;
+using System.Net;
+class Program
+{
+    static void Main (string [] args)
+    {
+        string cadena = "hola";
+        char letra = 'h';
+        Console.WriteLine("Esto es una cadena o string:" + cadena);
+        Console.WriteLine("Esto es un caracter o char:" + letra);
+    }
+
+}
