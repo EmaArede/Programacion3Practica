e3e6198402be06f7be797514ce39820e5c096240
Emanuel Arede Suárez <eearedesuarez@gmail.com>
2026-04-10 20:37:39 -0300

    Ajustes en 01_2 y 01_5

--- Ejercicios/prgoram01/program01_5.cs
+++ Ejercicios/prgoram01/program01_5.cs
@@ -1,16 +1,16 @@
-// 2. Registro de un solo usuario por medio de dni y legajo, con validación de los mismo
+// 5. Registro de un solo usuario por medio de DNI y legajo, con validación de los mismos
 
  Console.WriteLine(" Sistema de Registro \n");
 
-            string  emailRegistrado = "";
-            string  contraseñaRegistrada = "";
+            string  dniRegistrado = "";
+            string  legajoRegistrado = "";
            
             Console.WriteLine("--- REGISTRO ---");
-            Console.Write("Ingresa tu email: ");
-            emailRegistrado = Console.ReadLine();
+            Console.Write("Ingresa tu dni: ");
+            dniRegistrado = Console.ReadLine();
 
-            Console.Write("Ingresa una contraseña: ");
-            contraseñaRegistrada = Console.ReadLine();
+            Console.Write("Ingresa una legajo: ");
+            legajoRegistrado = Console.ReadLine();
 
             Console.WriteLine("\n✅ ¡Registro exitoso!\n");
 
@@ -21,13 +21,13 @@
 
             while (intentos > 0 && !accesoCorrecto)
             {
-                Console.Write("Email: ");
-                string emailIngresado = Console.ReadLine();
+                Console.Write("DNI: ");
+                string dniIngresado = Console.ReadLine();
 
-                Console.Write("Contraseña: ");
-                string contraseñaIngresada = Console.ReadLine();
+                Console.Write("legajo: ");
+                string legajoIngresado = Console.ReadLine();
 
-                if (emailIngresado == emailRegistrado && contraseñaIngresada == contraseñaRegistrada)
+                if (dniIngresado == dniRegistrado && legajoIngresado == legajoRegistrado)
                 {
                     accesoCorrecto = true;
                     Console.WriteLine($"\n ¡Bienvenido! Acceso concedido.");
@@ -36,7 +36,7 @@
                 {
                     intentos--;
                     if (intentos > 0)
-                        Console.WriteLine($"\n Email o contraseña incorrectos. Te quedan {intentos} intentos.\n");
+                        Console.WriteLine($"\n DNI o legajo incorrectos. Te quedan {intentos} intentos.\n");
                     else
                         Console.WriteLine("\n Acceso bloqueado. Demasiados intentos fallidos.");
                 }
--- Ejercicios/program00/program00.cs
+++ Ejercicios/program00/program00.cs
@@ -1,14 +1,21 @@
+// 1
 Console.WriteLine("Hola Mundo");
+// 2
 Console.WriteLine("mi nombre es: ");
 Console.ForegroundColor=ConsoleColor.Green;
+// 3
 Console.WriteLine("Emanuel Arede Suárez");
 Console.ResetColor();
+// 4
 Console.WriteLine("Estoy cursando:");
 Console.ForegroundColor=ConsoleColor.Yellow;
+// 5
 Console.WriteLine("Programación 3");
 Console.ResetColor();
+// 6
 Console.WriteLine("De la Carrera :");
 Console.ForegroundColor=ConsoleColor.Cyan;
+// 7
 Console.WriteLine("Tecnicatura Universitaria en Programación");
 Console.ResetColor();
 Console.WriteLine("Muchas Gracias");
