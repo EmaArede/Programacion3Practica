f88c9e7c8e9891d64e47dfb786c6edbd67a8e3bc
Emanuel Arede Suárez <eearedesuarez@gmail.com>
2026-04-10 12:48:07 -0300

    registro de pago

---
+++ Ejercicios/program05/program05_4.cs
@@ -0,0 +1,19 @@
+using System;
+namespace VerificaciónDePago;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("El usuario pagó?");
+
+            
+            bool PagoRegistrado = true;
+
+            Console.WriteLine("   ¿El usuario ha pagado? " + PagoRegistrado);
+
+            Console.WriteLine("\n presione cualquier tecla para salir");
+            Console.ReadLine();
+
+    }
+}
