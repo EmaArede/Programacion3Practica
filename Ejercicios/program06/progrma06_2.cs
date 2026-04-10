770d604b9fd057d816ad2d57aed82a9f2787f3bb
Emanuel Arede Suárez <eearedesuarez@gmail.com>
2026-04-10 12:53:57 -0300

    sistema en mantenimiento

---
+++ Ejercicios/program06/progrma06_2.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace ConstantesBooleanas
+{
+    class Program
+    {
+        // Constantes booleanas
+        const bool EDAD_MINIMA_REQUERIDA = true;
+        const bool VERIFICACION_DOS_FACTORES = true;
+        const bool MANTENIMIENTO_SISTEMA = true;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mantenimiento de Sistema\n");
+
+            Console.WriteLine($" ¿Edad mínima requerida? {EDAD_MINIMA_REQUERIDA}");
+            Console.WriteLine($" ¿Verificación dos factores? {VERIFICACION_DOS_FACTORES}");
+            Console.WriteLine($" ¿Sistema en mantenimiento? {MANTENIMIENTO_SISTEMA}");
+
+            if (MANTENIMIENTO_SISTEMA)
+            {
+                Console.WriteLine("\n El sistema está en mantenimiento. Intenta más tarde.");
+            }
+            else
+            {
+                Console.WriteLine("\n Sistema operativo normal.");
+            }
+        }
+    }
+}
