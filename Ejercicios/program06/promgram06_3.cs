f0d01a8e7eead8e97e064c62f3ef36d9c062afad
Emanuel Arede Suárez <eearedesuarez@gmail.com>
2026-04-10 12:57:41 -0300

    verificación de edad con constante

---
+++ Ejercicios/program06/promgram06_3.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace ConstantesBooleanas
+{
+    class Program
+    {
+        // Constantes booleanas
+        const bool EDAD_MINIMA_REQUERIDA = true;
+        const bool VERIFICACION_DOS_FACTORES = true;
+        const bool MANTENIMIENTO_SISTEMA = false;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mantenimiento de Sistema\n");
+
+            Console.WriteLine($" ¿Edad mínima requerida? {EDAD_MINIMA_REQUERIDA}");
+            Console.WriteLine($" ¿Verificación dos factores? {VERIFICACION_DOS_FACTORES}");
+            Console.WriteLine($" ¿Sistema en mantenimiento? {MANTENIMIENTO_SISTEMA}");
+
+            if (EDAD_MINIMA_REQUERIDA)
+            {
+                Console.WriteLine("\n No cumple con la edad mínima requerida");
+            }
+            else
+            {
+                Console.WriteLine("\n Acceso concedido.");
+            }
+        }
+    }
+}
