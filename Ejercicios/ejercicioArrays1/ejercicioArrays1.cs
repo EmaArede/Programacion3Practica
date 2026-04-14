5bb8c26b4e6835c7da5c335b78d0a842dcfa5287
Emanuel Arede Suárez <eearedesuarez@gmail.com>
2026-04-14 10:56:15 -0300

    Ejercicio con Arrays 1

---
+++ Ejercicios/ejercicioArrays1/ejercicioArrays1.cs
@@ -0,0 +1,74 @@
+namespace EjercicioArrays1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ejercicio 1: Fundamentos con Arreglos");
+            int [] numeros = new int [5];
+            numeros [0] = 10;
+            numeros [1] = 20;
+            numeros [2] = 30;
+            numeros [3] = 40;
+            numeros [4] = 50;
+            Console.WriteLine("Valores en índice 0:" + numeros[0]);
+            Console.WriteLine("Valores en índice 1:" + numeros[1]);
+            Console.WriteLine("Valores en índice 2:" + numeros[2]);
+            Console.WriteLine("Valores en índice 3:" + numeros[3]);
+            Console.WriteLine("Valores en índice 4:" + numeros[4]);
+            
+            // Ejercicio 2
+            Console.WriteLine("Ejercicio2");
+            int [] numeros2= new int [3];
+            numeros2 [0] = 5;
+            numeros2 [1] = 15;
+            numeros2 [2] = 25;
+            Console.WriteLine("Valor en índice 0:" + numeros2[0]);
+            Console.WriteLine("Valor en índice 1:" + numeros2[1]);
+            Console.WriteLine("Valor en índice 2:" + numeros2[2]);
+            // Ejercicio 3
+            Console.WriteLine("Ejercicio 3");
+            int [] numeros3 = new int [4];
+            numeros3 [0] = 2;
+            numeros3 [1] = 4;
+            numeros3 [2] = 6;
+            numeros3 [3] = 8;
+             Console.WriteLine("Valores en índice 0:" + numeros3[0]);
+            Console.WriteLine("Valores en índice 1:" + numeros3[1]);
+            Console.WriteLine("Valores en índice 2:" + numeros3[2]);
+            Console.WriteLine("Valores en índice 3:" + numeros3[3]);
+            
+            //Ejercicio 4
+            Console.WriteLine("Ejercicio 4");
+            int [] numeros4 = new int [5];
+            numeros4 [0] = 3;
+            numeros4 [1] = 6;
+            numeros4 [2] = 9;
+            numeros4 [3] = 12;
+            numeros4 [4] = 15;
+            Console.WriteLine ("Valores en índice 0:" + numeros4[0]);
+            Console.WriteLine ("Valores en índice 1:" + numeros4[1]);
+            Console.WriteLine ("Valores en índice 2:" + numeros4[2]);
+            Console.WriteLine ("Valores en índice 3:" + numeros4[3]);
+            Console.WriteLine ("Valores en índice 4:" + numeros4[4]);
+
+            // Ejercicio 5
+            Console.WriteLine ("Ejercicio 5");
+            int [] numeros5 = new int [6];
+            numeros5 [0] = 1;
+            numeros5 [1] = 3;
+            numeros5 [2] = 5;
+            numeros5 [3] = 7;
+            numeros5 [4] = 11;
+            numeros5 [5] = 13;
+            Console.WriteLine ("Valores en índice 0:" + numeros5[0]);
+            Console.WriteLine ("Valores en índice 1:" + numeros5[1]);
+            Console.WriteLine ("Valores en índice 2:" + numeros5[2]);
+            Console.WriteLine ("Valores en índice 3:" + numeros5[3]);
+            Console.WriteLine ("Valores en índice 4:" + numeros5[4]);
+            Console.WriteLine ("Valores en índice 5:" + numeros5[5]);
+        }
+
+    }
+
+}
